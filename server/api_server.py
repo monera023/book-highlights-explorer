45957d2b15b293727fbd5d9d05dd93a7dfd84cee
monera023 <sjituri@linkedin.com>
2024-06-21 11:26:44 +0530

    react frontend and v2 apis for it

--- server/api_server.py
+++ server/api_server.py
@@ -1,6 +1,7 @@
 import shutil
 import os
 from typing import Optional, List
+from fastapi.middleware.cors import CORSMiddleware
 
 from fastapi import FastAPI, File, UploadFile, Form, Query, Request
 from fastapi.templating import Jinja2Templates
@@ -16,6 +17,13 @@ highlights_processor = HighLightsFileProcessor()
 
 all_books = []
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def app_init():
@@ -68,13 +76,57 @@ async def fetch_highlights(request: Request):
 @app.get('/v1/searchHighlights')
 async def search(request: Request, query: Optional[str] = Query(None, description="Search terms"), books: Optional[List[str]] = Query(None, description="Selected books")):
     print(f"Got book query:: {books}")
-    # all_books = ["Bandhan : The Making of a Bank", "Vikram Sarabhai a life"]
     search_results = highlights_processor.search_highlights(query) if query else []
 
     filtered_results = [row for row in search_results if row[0] in books] if books else search_results
 
     print(f"Got response {len(filtered_results)}")
     return templates.TemplateResponse("search.html", {"request": request, "search_results": filtered_results, "book_names": all_books})
+
+@app.post("/v2/uploadHighlights")
+async def upload_highlights_v2(file: UploadFile = File(...),
+                            book_name: str = Form(...),
+                            author: str = Form(...),
+                            year: str = Form(...)):
+    uploaded_file_location = f"{AppConstants.UPLOADED_FILE_DIR}/{file.filename}"
+    print(f"Saving file .. {file.filename}")
+    with open(uploaded_file_location, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    print(f"Reading file {file.filename} for parsing..")
+    with open(uploaded_file_location, "r", encoding="utf-8") as f:
+        file_content = f.read()
+
+    highlights_data = highlights_processor.parse_highlights_from_file(file_content)
+    print(f"Parsed highlights.. got count {len(highlights_data)}")
+    highlights_processor.store_highlights(HighlightsMetadata(highlights_data, author, book_name, year))
+    print(f"Stored highlights in database..")
+
+    if os.path.exists(uploaded_file_location):
+        os.remove(uploaded_file_location)
+        print("Removed intermediate file..")
+    else:
+        print("Failed to remove file..")
+    return {
+        "response": "Highlights Stored successfully.."
+    }
+
+
+@app.get("/v2/fetchHighlights")
+async def fetch_highlights_v2(request: Request):
+    highlights = highlights_processor.fetch_highlights()
+    return highlights
+
+
+@app.get('/v2/searchHighlights')
+async def search_v2(request: Request, query: Optional[str] = Query(None, description="Search terms"), books: Optional[List[str]] = Query(None, description="Selected books")):
+    print(f"Got book query:: {books}")
+    search_results = highlights_processor.search_highlights(query) if query else []
+
+    filtered_results = [row for row in search_results if row[0] in books] if books else search_results
+
+    print(f"Got response {len(filtered_results)}")
+    return filtered_results
 
 
 if __name__ == "__main__":
